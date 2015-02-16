225a0cfd61e7b869ff18fed3df62769880fd55f8
PinkelPunk <PinkelPunk******>
2015-02-16 12:35:43 +0100

    klasse organisation erstellt

---
+++ BuilderAufg/src/orgaStruktur/Organisation.java
@@ -0,0 +1,18 @@
+package orgaStruktur;
+
+import orgaMerkmale.*;
+
+public class Organisation
+{
+	private EAbteilungen abteilung; //jede abteilung muss einen abteilungsleiter haben, es darf aber nur einer pro abteilung sein
+									//jede abteilung ausser die IT darf einen admin haben
+									//jede abteilung darf max. einen assistenten haben
+									//fakura + recht dürfen nur dann einen assistenten haben, wenn sie auch einen admin haben
+	
+	private EMitarbeiter mitarbeiter; //es müssen mind. 2 + dürfen max. 10 mitarbeiter pro abteilung sein
+	
+	
+	
+	
+	
+}
