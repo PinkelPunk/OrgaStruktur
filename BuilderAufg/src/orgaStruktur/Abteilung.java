cb0442d60b8decb1be8ab9730f4c062891a489ee
PinkelPunk <PinkelPunk******>
2015-02-17 08:52:17 +0100

    umbau x.x

--- BuilderAufg/src/orgaStruktur/Abteilung.java
+++ BuilderAufg/src/orgaStruktur/Abteilung.java
@@ -1,7 +1,16 @@
 package orgaStruktur;
 
+import orgaMerkmale.*;
+
 public class Abteilung extends AbteilungComponent
 {
+	private EAbteilungen abteilung; //jede abteilung muss einen abteilungsleiter haben, es darf aber nur einer pro abteilung sein
+									//jede abteilung ausser die IT darf einen admin haben
+									//jede abteilung darf max. einen assistenten haben
+									//fakura + recht dürfen nur dann einen assistenten haben, wenn sie auch einen admin haben
+
+	private EMitarbeiter mitarbeiter; //es müssen mind. 2 + dürfen max. 10 mitarbeiter pro abteilung sein
+	
 	private String name;
 	private AbteilungComponent[] oc;
 	private int anzAbteilungen;
@@ -34,5 +43,16 @@ public class Abteilung extends AbteilungComponent
 	public String toString()
 	{
 		return "Abteilung: " + getName();
+	}
+	
+	public class AbteilungBuilder
+	{
+		private EAbteilungen abteilung; //jede abteilung muss einen abteilungsleiter haben, es darf aber nur einer pro abteilung sein
+										//jede abteilung ausser die IT darf einen admin haben
+										//jede abteilung darf max. einen assistenten haben
+										//fakura + recht dürfen nur dann einen assistenten haben, wenn sie auch einen admin haben
+
+		private EMitarbeiter mitarbeiter; //es müssen mind. 2 + dürfen max. 10 mitarbeiter pro abteilung sein
+		
 	}
 }
